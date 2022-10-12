8e21eea8af3f062d49c07933d1c4c0b1fa4859df
Chris <chrismanfredijr@gmail.com>
2022-10-12 17:46:14 -0400

    intial project styling

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -8,12 +8,25 @@ function Projects() {
 
             <div className="projectCard">
                 <div className='imgWrapper'>
-                    <img src={tastemakr} alt="tastemakr website" width="500" height="300" className='projectImg'/>
+                    <img src={tastemakr} alt="tastemakr website" width="600" height="360" className='projectImg' />
                 </div>
-               
+
                 <div className='projectInfo'>
                     <h3 className="projectTitle">Tastemakr</h3>
-                    <p>Tastemakr is a search engine which uses the LAST.FM API to generate music recommendations based on user input. Users can log in and save recommendations to the database</p>
+                    <p className='projectBio'>Tastemakr is a search engine which uses the LAST.FM API to generate music recommendations based on user input. Users can log in and save recommendations to the database</p>
+                    <div className='builtWith'>
+                        <ul className="builtWithList">
+                            <li>React</li>
+                            <li>Express</li>
+                            <li>Node</li>
+                            <li>Mongo</li>
+                        </ul>
+                        <ul className="builtWithList">
+                            <li>LAST.FM api</li>
+                            <li>React Boostrap</li>
+                            <li>GraphQL</li>
+                        </ul>
+                    </div>
                     <div className='projectBtnWrapper'>
                         <Button variant="outline-light" className="projectButton">
                             visit site
@@ -23,12 +36,68 @@ function Projects() {
                         </Button>
                     </div>
                 </div>
-                
-            </div>
-            <div className="projectCard">
 
             </div>
             <div className="projectCard">
+                <div className='projectInfo'>
+                    <h3 className="projectTitle">Tastemakr</h3>
+                    <p className='projectBio'>Tastemakr is a search engine which uses the LAST.FM API to generate music recommendations based on user input. Users can log in and save recommendations to the database</p>
+                    <div className='builtWithRight'>
+                        <ul className="builtWithList">
+                            <li>React</li>
+                            <li>Express</li>
+                            <li>Node</li>
+                            <li>Mongo</li>
+                        </ul>
+                        <ul className="builtWithList">
+                            <li>LAST.FM api</li>
+                            <li>React Boostrap</li>
+                            <li>GraphQL</li>
+                        </ul>
+                    </div>
+                    <div className='projectBtnWrapper'>
+                        <Button variant="outline-light" className="projectButton">
+                            visit site
+                        </Button>
+                        <Button variant="outline-light" className="projectButton">
+                            view code
+                        </Button>
+                    </div>
+                </div>
+                <div className='imgWrapper'>
+                    <img src={tastemakr} alt="tastemakr website" width="600" height="360" className='projectImgRight' />
+                </div>
+            </div>
+            <div className="projectCard">
+            <div className='imgWrapper'>
+                    <img src={tastemakr} alt="tastemakr website" width="600" height="360" className='projectImg' />
+                </div>
+
+                <div className='projectInfo'>
+                    <h3 className="projectTitle">Tastemakr</h3>
+                    <p className='projectBio'>Tastemakr is a search engine which uses the LAST.FM API to generate music recommendations based on user input. Users can log in and save recommendations to the database</p>
+                    <div className='builtWith'>
+                        <ul className="builtWithList">
+                            <li>React</li>
+                            <li>Express</li>
+                            <li>Node</li>
+                            <li>Mongo</li>
+                        </ul>
+                        <ul className="builtWithList">
+                            <li>LAST.FM api</li>
+                            <li>React Boostrap</li>
+                            <li>GraphQL</li>
+                        </ul>
+                    </div>
+                    <div className='projectBtnWrapper'>
+                        <Button variant="outline-light" className="projectButton">
+                            visit site
+                        </Button>
+                        <Button variant="outline-light" className="projectButton">
+                            view code
+                        </Button>
+                    </div>
+                </div>
 
             </div>
 
