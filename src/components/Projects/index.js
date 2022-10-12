275e510c78a186e21b591ad8ab3506b803f1271f
Chris <chrismanfredijr@gmail.com>
2022-10-12 16:20:55 -0400

    intial project card setup

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,29 @@
 import './index.css'
+import tastemakr from '../../assets/images/tastemakr.png'
+import Button from 'react-bootstrap/Button';
 function Projects() {
     return (
         <div className="Projects section" id="projects">
-            <h2 className='title projectTitle'>My Projects:</h2>
+            <h2 className='title projectSection'>My Projects:</h2>
 
             <div className="projectCard">
-
+                <div className='imgWrapper'>
+                    <img src={tastemakr} alt="tastemakr website" width="500" height="300" className='projectImg'/>
+                </div>
+               
+                <div className='projectInfo'>
+                    <h3 className="projectTitle">Tastemakr</h3>
+                    <p>Tastemakr is a search engine which uses the LAST.FM API to generate music recommendations based on user input. Users can log in and save recommendations to the database</p>
+                    <div className='projectBtnWrapper'>
+                        <Button variant="outline-light" className="projectButton">
+                            visit site
+                        </Button>
+                        <Button variant="outline-light" className="projectButton">
+                            view code
+                        </Button>
+                    </div>
+                </div>
+                
             </div>
             <div className="projectCard">
 
