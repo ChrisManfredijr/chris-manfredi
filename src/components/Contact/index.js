8ad2ba3a1c49c78146f3aa64585bcffc7ad22760
Chris <chrismanfredijr@gmail.com>
2022-10-12 21:34:44 -0400

    contact form validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,7 +4,22 @@ import { Link } from 'react-scroll';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { FaGithubSquare, FaLinkedin, FaAngleDoubleUp } from "react-icons/fa";
+import emailjs from "emailjs-com";
+
 function Contact() {
+    function sendEmail(e) {
+        e.preventDefault();
+        console.log(e.target);
+        emailjs.sendForm('gmail', 'template_nzgxzjn', e.target, 'b-HdTsNp9POAZmT8W')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+            e.target.reset()
+    }
+    
     return (
        
         <div className="Contact section" id="contact">
@@ -15,18 +30,22 @@ function Contact() {
                 <a href="https://www.linkedin.com/in/chris-manfredi-3b09b5114/" target="_blank" rel="noopener noreferrer"><FaLinkedin className="linkedinHome" /></a>
             </div>
             
-            <Form className='contactInput'>
+            <Form className='contactInput' onSubmit={sendEmail}>
                 <h3 className='sendMessage'>Send me a message:</h3>
-                <Form.Group className="mb-2" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" />
+                <Form.Group className="mb-2" controlId="formBasicEmail" >
+                    <Form.Control type="name" placeholder="Name" name="name"/>
+                </Form.Group>
+    
+                <Form.Group className="mb-2" controlId="formBasicEmail" >
+                    <Form.Control type="email" placeholder="Email" name="email"/>
                 </Form.Group>
 
                 <Form.Group className="mb-2" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="subject" />
+                    <Form.Control type="subject" placeholder="Subject" name="subject"/>
                 </Form.Group>
                 
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Control as="textarea" rows={8} placeholder="message"/>
+                    <Form.Control as="textarea" rows={8} placeholder="Message" name="message"/>
                 </Form.Group>
                 <div className='btnWrapper'>
                     <Button variant="outline-light" type="submit" className="btn">
