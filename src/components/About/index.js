fb199ca7d66254b6085144a65867a6c17a73220f
Chris <chrismanfredijr@gmail.com>
2023-01-10 14:57:37 -0500

    new about me paragraph

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,20 +4,27 @@ import CV from '../../assets/ChrisManfredi.pdf'
 
 
 function About() {
-    
-   
-    
     return (
-     
         <div className="About section" id='about'>
             <h2 className="title aboutTitle">About Me</h2>
-            <div className="photoWrapper">
-                <img src={Chris} alt= "Chris Manfredi, Fullstack Developer" width="295" height="435" className="photo"/>
+            
+                
+                <div className="photoWrapper">
+                    <img src={Chris} alt="Chris Manfredi, Fullstack Developer" width="295" height="435" className="photo" />
+                </div>
+
+                <div className="paragraphWrapper">
+                    <p className="aboutParagraph">My name is Chris Manfredi and I am a frontend developer with a background in business data analytics. I graduated from the University of Connecticut where I developed an interest in coding and decided to pursue it further. To gain the technical skills I needed, I attended a coding bootcamp and learned the fundamentals of web development. I also served in the United States Marine Corps Reserve where I learned discipline, attention to detail and the ability to work in a team, all of which have helped me to become a better developer. In my free time, I enjoy playing guitar and snowboarding. With my unique set of skills, I am excited to use my passion for coding to build high-quality and user-friendly websites.
+                    </p>
+
+                    <br/>
+                    <div className="aboutBtnWrapper">
+                        <a href={CV} target="_blank" rel="noopener noreferrer" className="underBtn underBtnPrj">
+                         VIEW RESUME
+                        </a>
+                    </div>
             </div>
-            <p className="aboutParagraph">My name is Chris Manfredi, I am web developer commited to life-long learning and constant improvement.</p>
-            <a href={CV} target="_blank" rel="noopener noreferrer" className="underBtn">
-                    RESUME
-            </a>        
+
             <div className="skillSection">
                 <div className="skillBox Frontend">
                     <ul className="skillList">
@@ -29,6 +36,7 @@ function About() {
                         <li>React</li>
                     </ul>
                 </div>
+
                 <div className="skillBox Backend">
                     <ul className="skillList">
                         <li className="nameWrapper"><h3 className="skillName">Backend</h3></li>
@@ -47,13 +55,11 @@ function About() {
                         <li>Object Oriented Programming</li>
                         <li>Model View Controller</li>
                         <li>MERN Stack</li>
-                       
+
                     </ul>
                 </div>
-
-                <div>
-                </div>
             </div>
+
             <div className="slant">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
@@ -62,17 +68,6 @@ function About() {
         </div>
     );
 }
-/*
-                <div className="skillBox">
-                    <ul className="skillList">
-                        <li><h3 className="skillName">Concepts</h3></li>
-                        <li>Test Driven Development</li>
-                        <li>Object Oriented Programming</li>
-                        <li>Model View Controller</li>
-                        <li>MERN Stack</li>
-                        <li>Model View Controller</li>
-                    </ul>
-                </div>
-*/
+
 
 export default About;
