89f34f302db60176913db0aaa987bad527166875
Chris <chrismanfredijr@gmail.com>
2023-01-13 23:25:52 -0500

    project bios

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -9,7 +9,7 @@ function Projects() {
             name: 'Connect Four',
             img: connectfour,
             bio: 'Play Connect Four against another player in the browser. Game will automatically detect wins and keep score. Save this React app as a Progressive Web Application',
-            tech: ['React', 'SPA', 'PWA', 'Game Design', 'JSX', 'CSS'],
+            tech: ['React ', 'SPA ', 'PWA ', 'Game Design ', 'JSX ', 'CSS '],
             site: 'https://connectfour-manfredi.netlify.app/',
             github: 'https://github.com/ChrisManfredijr/connectfour'
         },
@@ -17,7 +17,7 @@ function Projects() {
             name: 'Tastemakr',
             img: tastemakr,
             bio: 'Generate music recommendations by entering an artist/band name. Login to save and view previously saved recommendations. MERN Stack application with REST api and GraphQL',
-            tech: ['React', 'Express', 'Node', 'MongoDB', 'Last.FM API', 'GraphQL', 'React Bootstrap'],
+            tech: ['React ', 'Express ', 'Node ', 'MongoDB ', 'Last.FM API ', 'GraphQL '],
             site: 'https://tastemakr.herokuapp.com/',
             github: 'https://github.com/ChrisManfredijr/tastemakr'
         },
@@ -25,30 +25,50 @@ function Projects() {
             name: 'Spotify Now',
             img: spotifynow,
             bio: 'Login to Spotify to view your top songs and artists for the year. Generate a custom playlist with your top songs. You can also share your playist to the public database. Integrates MVC architecture design patterns and 0Auth.',
-            tech: ['HTML', 'CSS', 'JavaScript', 'Handlebars', 'Node', 'Express', 'Spotify-web-api-node', 'MySql', 'Sequelize', 'Bootstrap'],
+            tech: ['HTML', ' CSS ', 'JavaScript ', 'Handlebars ', 'REST API  ',  'OAuth ', 'MySql '],
             site: 'https://spotify-now-app.herokuapp.com/',
             github: 'https://github.com/ajjaswal/spotify-now'
         },
 
     ]
-      
-    
+
+
     return (
         <div className="Projects section" id="projects">
-            <h2 className='title'>My Projects:</h2>
-            
-            <div className='projectSection'>
-            {projectArray.map(() => {
-                return(
-                <div className='projectCardWrapper'>
-                    <div className='projectCard'>
-                    </div>
-                </div>
-                );
+            <h2 className='title ProjectTitle'>My Projects:</h2>
 
-            })}
+            <div className='projectSection'>
+                {projectArray.map((card, index) => {
+                    return (
+                        <div className='projectCardWrapper' key={index}>
+                            <div className='projectCard'>
+                                <div className='imgWrapper'>
+                                    <img src={card.img} width="640" height="360" className='projectImg' />
+                                </div>
+                            
+                                <div className='ProjectInfo'>
+                                    <h3 className='projectName'>{card.name}</h3>
+                                    <p className='projectBio'>{card.bio}</p>
+                                    
+                                    <div className='techStack'>
+                                        {card.tech}
+                                    </div>
+                                    <div className='projectButtonWrapper'>
+                                        <a href={card.site} target="_blank" rel="noopener noreferrer" className='underBtn underPrj'>
+                                        VISIT SITE
+                                        </a>
+                                        <a href={card.github} target="_blank" rel="noopner noreferrer" className='underBtn underPrj'>
+                                        VIEW CODE
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    );
+
+                })}
             </div>
-            
+
             <div className="triangleLight triangleProjects">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" className="shape-fill"></path>
