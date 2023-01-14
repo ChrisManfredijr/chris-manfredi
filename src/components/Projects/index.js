c04076bcff103d6f5e6e0e3b17cfb8bcf09534b2
Chris <chrismanfredijr@gmail.com>
2023-01-13 19:29:44 -0500

    intial project section wireframe

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,113 +4,51 @@ import connectfour from '../../assets/images/connectfour.webp';
 import spotifynow from '../../assets/images/spotifynow.webp';
 
 function Projects() {
+    const projectArray = [
+        {
+            name: 'Connect Four',
+            img: connectfour,
+            bio: 'Play Connect Four against another player in the browser. Game will automatically detect wins and keep score. Save this React app as a Progressive Web Application',
+            tech: ['React', 'SPA', 'PWA', 'Game Design', 'JSX', 'CSS'],
+            site: 'https://connectfour-manfredi.netlify.app/',
+            github: 'https://github.com/ChrisManfredijr/connectfour'
+        },
+        {
+            name: 'Tastemakr',
+            img: tastemakr,
+            bio: 'Generate music recommendations by entering an artist/band name. Login to save and view previously saved recommendations. MERN Stack application with REST api and GraphQL',
+            tech: ['React', 'Express', 'Node', 'MongoDB', 'Last.FM API', 'GraphQL', 'React Bootstrap'],
+            site: 'https://tastemakr.herokuapp.com/',
+            github: 'https://github.com/ChrisManfredijr/tastemakr'
+        },
+        {
+            name: 'Spotify Now',
+            img: spotifynow,
+            bio: 'Login to Spotify to view your top songs and artists for the year. Generate a custom playlist with your top songs. You can also share your playist to the public database. Integrates MVC architecture design patterns and 0Auth.',
+            tech: ['HTML', 'CSS', 'JavaScript', 'Handlebars', 'Node', 'Express', 'Spotify-web-api-node', 'MySql', 'Sequelize', 'Bootstrap'],
+            site: 'https://spotify-now-app.herokuapp.com/',
+            github: 'https://github.com/ajjaswal/spotify-now'
+        },
+
+    ]
+      
+    
     return (
         <div className="Projects section" id="projects">
-
-            <h2 className='title projectSection'>My Projects:</h2>
-
-            <div className="projectCard">
-                <div className='imgWrapper'>
-                    <img src={connectfour} alt="tastemakr website" width="640" height="360" className='projectImg' />
-                </div>
-
-                <div className='projectInfo'>
-                    <h3 className="projectTitle">Connect Four</h3>
-                    <p className='projectBio'>Play Connect Four against another player in the browser. Game will automatically detect wins and keep score. Save this React app as a Progressive Web Application</p>
-                    <div className='builtWith'>
-                        <ul className="builtWithList">
-                            <li>React</li>
-                            <li>SPA</li>
-                            <li>PWA</li>
-
-                        </ul>
-                        <ul className="builtWithList">
-                            <li>Game design</li>
-                            <li>JSX</li>
-                            <li>CSS</li>
-                        </ul>
-                    </div>
-                    <div className='projectBtnWrapper'>
-                        <a href="https://connectfour-manfredi.netlify.app/" target="_blank" rel="noopener noreferrer" className='underBtn underPrj'>
-                            VIEW SITE
-                        </a>
-                        <a href="https://github.com/ChrisManfredijr/connectfour" target="_blank" rel="noopner noreferrer" className='underBtn underPrj'> 
-                            VIEW CODE
-                        </a>
+            <h2 className='title'>My Projects:</h2>
+            
+            <div className='projectSection'>
+            {projectArray.map(() => {
+                return(
+                <div className='projectCardWrapper'>
+                    <div className='projectCard'>
                     </div>
                 </div>
+                );
 
+            })}
             </div>
-            <div className="projectCard">
-                <div className='imgWrapper smallScreenImg'>
-                    <img src={tastemakr} alt="tastemakr website" width="640" height="360" className='projectImg projectImgRight' />
-                </div>
-
-                <div className='projectInfo projectInfoRight'>
-                    <h3 className="projectTitle">Tastemakr</h3>
-                    <p className='projectBio'>Generate music recommendations by entering an artist/band name. Login to save and view previously saved recommendations. MERN Stack application with REST api and GraphQL</p>
-                    <div className='builtWithRight'>
-                        <ul className="builtWithList">
-                            <li>React</li>
-                            <li>Express</li>
-                            <li>Node</li>
-                            <li>Mongo</li>
-                        </ul>
-                        <ul className="builtWithList">
-                            <li>LAST.FM api</li>
-                            <li>React Boostrap</li>
-                            <li>GraphQL</li>
-                        </ul>
-                    </div>
-                    <div className='projectBtnWrapper'>
-                        <a href="https://tastemakr.herokuapp.com/" target="_blank" rel="noopener noreferrer" className='underBtn underPrj'>
-                                VISIT SITE
-                        </a>
-                        <a href="https://github.com/ChrisManfredijr/tastemakr" target="_blank" rel="noopner noreferrer" className='underBtn underPrj'>
-                            VIEW CODE
-                        </a>
-                    </div>
-                </div>
-                <div className='imgWrapper largeScreenImg'>
-                    <img src={tastemakr} alt="tastemakr website" width="640" height="360" className='projectImgRight' />
-                </div>
-            </div>
-            <div  className="projectCard">
-                <div className='imgWrapper'>
-                    <img src={spotifynow} alt="tastemakr website" width="640" height="360" className='projectImg' />
-                </div>
-
-                <div className='projectInfo'>
-                    <h3 className="projectTitle">Spotify Now</h3>
-                    <p className='projectBio'>Login to Spotify to view your top songs and artists for the year. Generate a custom playlist with your top songs. You can also share your playist to the public database. Integrates MVC architecture design patterns and 0Auth.</p>
-                    <div className='builtWith'>
-                        <ul className="builtWithList">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                            <li>Bootstrap</li>
-                            <li>Handlebars</li>
-                        </ul>
-                        <ul className="builtWithList">
-                            <li>Node</li>
-                            <li>Express</li>
-                            <li>spotify-web-api-node</li>
-                            <li>MySql</li>
-                            <li>Sequelize</li>
-                        </ul>
-                    </div>
-                    <div className='projectBtnWrapper'>
-                        <a href="https://spotify-now-app.herokuapp.com/" target="_blank" rel="noopener noreferrer" className='underBtn underPrj'>
-                            VISIT SITE
-                        </a>
-                        <a href="https://github.com/ajjaswal/spotify-now" target="_blank" rel="noopner noreferrer" className='underBtn underPrj'>
-                            VIEW CODE
-                        </a>
-                    </div>
-                </div>
-
-            </div>
-
+            
             <div className="triangleLight triangleProjects">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" className="shape-fill"></path>
@@ -120,5 +58,25 @@ function Projects() {
         </div>
     )
 }
+
+/*
+ <img src={connectfour} alt="tastemakr website" width="640" height="360" className='' />
+ <img src={tastemakr} alt="tastemakr website" width="640" height="360" className='' />
+<img src={spotifynow} alt="tastemakr website" width="640" height="360" className='' />
+ <a href="https://tastemakr.herokuapp.com/" target="_blank" rel="noopener noreferrer" className='underBtn underPrj'>
+                                VISIT SITE
+                        </a>
+                        <a href="https://github.com/ChrisManfredijr/tastemakr" target="_blank" rel="noopner noreferrer" className='underBtn underPrj'>
+                            VIEW CODE
+                        </a>
+
+<a href="https://spotify-now-app.herokuapp.com/" target="_blank" rel="noopener noreferrer" className='underBtn underPrj'>
+                            VISIT SITE
+                        </a>
+                        <a href="https://github.com/ajjaswal/spotify-now" target="_blank" rel="noopner noreferrer" className='underBtn underPrj'>
+                            VIEW CODE
+                        </a>
+
+*/
 
 export default Projects;
